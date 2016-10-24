804170169cc089021f35ef6eab8e1302cfed8950
Chien Vu <chien.vh@gmail.com>
2016-10-24 15:44:06 +0700

    Initial commit

---
+++ CalculationProjectTest/MyCalculationTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalculationProject;
+
+namespace CalculationProjectTest
+{
+    [TestClass]
+    public class MyCalculationTest
+    {
+        [TestMethod]
+        public void TestAdd()
+        {
+            var myCalculation = new MyCalculation();
+            int result = myCalculation.Add(5, 5);
+            Assert.AreEqual(10, result);
+        }
+    }
+}
